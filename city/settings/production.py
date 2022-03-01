3e086586dbe219b0db3485af8438d702116225fe
shumeevfoto <48292529+shumeevfoto@users.noreply.github.com>
2022-03-01 15:35:40 +0300

    Update production.py

--- city/settings/production.py
+++ city/settings/production.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -140,3 +140,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+
+django_heroku.settings(locals())
